8742523e1f31a2fb859e9e86d471c8a1668fede1
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-11 11:50:31 -0500

    builds out routes to views on clientapp.js

--- client/scripts/clientapp.js
+++ client/scripts/clientapp.js
@@ -7,23 +7,82 @@ myApp.config(['$routeProvider', '$locationProvider',
 
   $routeProvider
 //login
-
+    .when('/login', {
+      templateUrl: '/views/templates/login.html',
+      controller: 'LoginController'
+    })
 
 //terms of use
+    .when('/terms', {
+      templateUrl: '/views/templates/termsOfUse.html',
+      controller: 'LoginController'
+    })
 
 //register
     .when('/register', {
       templateUrl: '/views/templates/register.html',
       controller: 'LoginController'
     })
+
 //home
     .when('/home', {
       templateUrl: '/views/templates/home.html',
       controller: 'LoginController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
     })
 
+//incomplete form
+    .when('/incomplete-form', {
+      templateUrl: '/views/templates/incompleteForm.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
 
-//extra
+//check in form
+  //lots of stuff here TBD
+
+//day detail
+    .when('/day-detail', {
+      templateUrl: '/views/templates/dayDetail.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+
+//graph summary
+    .when('/graph-summary', {
+      templateUrl: '/views/templates/graphSummary.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+
+//admin export
+    .when('/admin-export', {
+      templateUrl: '/views/templates/adminExport.html',
+      controller: 'InfoController',
+      resolve: {
+        getuser : ['UserService', function(UserService){
+          return UserService.getuser();
+        }]
+      }
+    })
+
+//extra from base project
     .when('/info', {
       templateUrl: '/views/templates/info.html',
       controller: 'InfoController',
