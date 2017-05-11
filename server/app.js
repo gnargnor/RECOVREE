2dcfdee5220a99520f39fee872bb6b37456597b3
Anisa <anisa.ali7@gmail.com>
2017-05-11 11:30:45 -0500

    database name in mongoURI

--- server/app.js
+++ server/app.js
@@ -40,13 +40,13 @@ app.use('/', index);
 // Mongo Connection //
 var mongoURI = '';
 // process.env.MONGODB_URI will only be defined if you
-// are running on Heroku
-if(process.env.MONGODB_URI != undefined) {
+// are running on Heroku - will define using mLab connection MONGODB_URI
+if(process.env.MONGODB_URI !== undefined) {
     // use the string value of the environment variable
     mongoURI = process.env.MONGODB_URI;
 } else {
     // use the local database server
-    mongoURI = 'mongodb://localhost:27017/passport';
+    mongoURI = 'mongodb://localhost:27017/recovree';
 }
 
 // var mongoURI = "mongodb://localhost:27017/passport";
