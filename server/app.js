9798e0a85eae88af6dcb5745701ab607acd32f0a
Anisa <anisa.ali7@gmail.com>
2017-05-11 11:52:37 -0500

    setup mongo db

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var path = require('path');
+var db = require("./models/db");
 
 var passport = require('./strategies/userStrategy');
 var session = require('express-session');
@@ -36,32 +37,6 @@ app.use(passport.session());
 app.use('/register', register);
 app.use('/user', user);
 app.use('/', index);
-
-// Mongo Connection //
-var mongoURI = '';
-// process.env.MONGODB_URI will only be defined if you
-// are running on Heroku
-if(process.env.MONGODB_URI != undefined) {
-    // use the string value of the environment variable
-    mongoURI = process.env.MONGODB_URI;
-} else {
-    // use the local database server
-    mongoURI = 'mongodb://localhost:27017/passport';
-}
-
-// var mongoURI = "mongodb://localhost:27017/passport";
-var mongoDB = mongoose.connect(mongoURI).connection;
-
-mongoDB.on('error', function(err){
-   if(err) {
-     console.log("MONGO ERROR: ", err);
-   }
-   res.sendStatus(500);
-});
-
-mongoDB.once('open', function(){
-   console.log("Connected to Mongo, meow!");
-});
 
 // App Set //
 app.set('port', (process.env.PORT || 5000));
