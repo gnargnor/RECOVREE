74506a132efaa95d02b5f1e3c390b1f19d322b77
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-11 11:32:00 -0500

    rearranges clientapp.js

--- client/scripts/clientapp.js
+++ client/scripts/clientapp.js
@@ -6,26 +6,36 @@ myApp.config(['$routeProvider', '$locationProvider',
   $locationProvider.hashPrefix('');
 
   $routeProvider
-    .when('/home', {
-      templateUrl: '/views/templates/home.html',
-      controller: 'LoginController',
-    })
+//login
+
+
+//terms of use
+
+//register
     .when('/register', {
       templateUrl: '/views/templates/register.html',
       controller: 'LoginController'
     })
-    .when('/user', {
-      templateUrl: '/views/templates/user.html',
-      controller: 'UserController',
+//home
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'LoginController',
+    })
+
+
+//extra
+    .when('/info', {
+      templateUrl: '/views/templates/info.html',
+      controller: 'InfoController',
       resolve: {
         getuser : ['UserService', function(UserService){
           return UserService.getuser();
         }]
       }
     })
-    .when('/info', {
-      templateUrl: '/views/templates/info.html',
-      controller: 'InfoController',
+    .when('/user', {
+      templateUrl: '/views/templates/user.html',
+      controller: 'UserController',
       resolve: {
         getuser : ['UserService', function(UserService){
           return UserService.getuser();
