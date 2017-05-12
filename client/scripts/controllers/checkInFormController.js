1c6e878cb6232e90a2089c7a8514d0b82d0c331f
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-12 15:57:41 -0500

    sets up nonvalidate dailyFeelingsForm

--- client/scripts/controllers/checkInFormController.js
+++ client/scripts/controllers/checkInFormController.js
@@ -1,21 +1,29 @@
 myApp.controller('CheckInFormController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
   console.log('CheckInFormController sourced!');
-  var feelingList = ['angry','anxious'];
-  var feelings = [];
-  var feeling = {};
-  var values = [ true, false];
-  feeling.name = '';
-  feeling.value = Boolean;
+  var feelings = [
+    {
+      name: 'angry',
+      value: false
+    },
+    {
+      name: 'anxious',
+      value: false
+    },
+    {
+      name: 'depressed',
+      value: false
+    },
+  ];
 
-  $scope.feelingList = feelingList;
-  $scope.angry = 'angry';
-  $scope.anxious = 'anxious';
-  $scope.feeling = feeling;
-  $scope.values = values;
+  var todaysFeelings = {};
 
-  $scope.nextButton = nextButton;
   function nextButton(){
     console.log("you clicked the next button");
     console.log("feeling object");
   }
+
+  $scope.feelings = feelings;
+  $scope.todaysFeelings = todaysFeelings;
+  $scope.todaysFeelings.feelings = feelings;
+  $scope.nextButton = nextButton;
 }]);
