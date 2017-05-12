3f8508bcdace01824dde492481d3a37b512925fa
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-12 14:19:41 -0500

    adds input to first daily Form  view

--- client/scripts/controllers/checkInFormController.js
+++ client/scripts/controllers/checkInFormController.js
@@ -1,3 +1,8 @@
 myApp.controller('CheckInFormController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
   console.log('CheckInFormController sourced!');
+
+  $scope.nextButton = nextButton;
+  function nextButton(){
+    console.log("you clicked the next button");
+  }
 }]);
