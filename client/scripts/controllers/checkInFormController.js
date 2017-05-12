22712d046eae8ec59fb5f393acee614eac8993c6
Keith Tomlinson <kbarretttomlinson@gmail.com>
2017-05-12 15:25:24 -0500

    starts to build form

--- client/scripts/controllers/checkInFormController.js
+++ client/scripts/controllers/checkInFormController.js
@@ -1,8 +1,21 @@
 myApp.controller('CheckInFormController', ['$scope', '$http', '$location', 'UserService', function($scope, $http, $location, UserService) {
   console.log('CheckInFormController sourced!');
+  var feelingList = ['angry','anxious'];
+  var feelings = [];
+  var feeling = {};
+  var values = [ true, false];
+  feeling.name = '';
+  feeling.value = Boolean;
+
+  $scope.feelingList = feelingList;
+  $scope.angry = 'angry';
+  $scope.anxious = 'anxious';
+  $scope.feeling = feeling;
+  $scope.values = values;
 
   $scope.nextButton = nextButton;
   function nextButton(){
     console.log("you clicked the next button");
+    console.log("feeling object");
   }
 }]);
